fa74d6ea4de8bcc8b732e21d73987980f6160b78
rafai123 <rafaimhd123@gmail.com>
2023-05-30 18:42:10 +0700

    add starter template for router, and add starter template for error element

---
+++ src/main.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import ErrorPage from './pages/ErrorPage.jsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      {/* Ini Layout biar tetep ada di semua halaman. Misalnya Navbar, Footer Bisa di taroh sini */}
+      <Route path="/" element={<App />} errorElement={<ErrorPage />} >
+        {/* Di bawah sini bisa di taroh tiap halaman yang kalian buat Contohnya :  */}
+        {/* <Route path="/home" element={<Home />} /> */}
+      
+      </Route>
+    </>
+  )
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* Semua Router yang kita buat di atas,  nanti bakalan di load di Router Provider di bawah ini */}
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,11 @@
+const ErrorPage = () => {
+    return (
+        <div>
+            <h1>404</h1>
+            <p>Page not found</p>
+        </div>
+        
+    )
+}
+
+export default ErrorPage
