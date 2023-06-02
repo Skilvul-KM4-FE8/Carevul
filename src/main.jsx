e5ca6b9bc99d079114b8acf006576c8a0bed7706
rafai123 <rafaimhd123@gmail.com>
2023-06-02 15:42:55 +0700

    add title for Pilih Dokter

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-// import './index.css'
+import './index.css'
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import ErrorPage from './pages/ErrorPage.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
--- src/pages/ListDoctor.jsx
+++ src/pages/ListDoctor.jsx
@@ -1,7 +1,16 @@
+import { Col, Container, Row } from "react-bootstrap"
+
 const ListDoctor = () => {
     return (
         <>
-            <h1>Testtahjsdjdas</h1>
+            <Container>
+                <Row>
+                    <Col className="text-start">
+                        <h2 className="text-carevul fw-bold">Rekomendasi Dokter</h2>
+                        <p className="text-secondary">Pilih dokter yang sesuai dengan kebutuhan kamu yuk</p>
+                    </Col>
+                </Row>
+            </Container>
         </>
     )
 }
