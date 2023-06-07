47ea6b0b13cf45a7600e20235e978ea5f409b6cf
Syifaaahdr29 <syifaaahdr29@gmail.com>
2023-06-07 15:36:07 +0700

    add article-home and article-read jsx

---
+++ src/pages/Article-Read.jsx
@@ -0,0 +1,3 @@
+import { Col, Container, Row, Button } from "react-bootstrap";
+import "./../styles/article-read.css";
+import { Link } from "react-router-dom";
---
+++ src/pages/ArticleHome.jsx
@@ -0,0 +1,3 @@
+import { Col, Container, Row, Button } from "react-bootstrap";
+import "./../styles/article-home.css";
+import { Link } from "react-router-dom";
