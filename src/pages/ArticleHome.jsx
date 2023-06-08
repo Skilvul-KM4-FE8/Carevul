4cff505e12ed439f22bc205e31b2eec6b922ad66
Syifaaahdr29 <syifaaahdr29@gmail.com>
2023-06-09 01:14:23 +0700

    add card

--- src/pages/ArticleHome.jsx
+++ src/pages/ArticleHome.jsx
@@ -1,17 +1,41 @@
 import { Col, Container, Row, Button } from "react-bootstrap";
 import "./../styles/article-home.css";
 import { Link } from "react-router-dom";
+import Card from 'react-bootstrap/Card';
 
 const ArticleHome = () => {
-    return (
-      <>
-        <Container>
+  return (
+    <>
+      <section id="card" className="article-card">
+        <Container fluid>
+          <div className="title-holder">
+            <h2>Artikel</h2>
+            <div>bacalah artikel ini bro</div>
+          </div>
           <Row>
-
+            {cardData.map((data) => {
+              return (
+                <Col sm={4} key={data.id}>
+                <div>
+                  <Card>
+                    <Card.Img variant="top" src={data.img} />
+                    <Card.Body>
+                      <Card.Title>{data.title}</Card.Title>
+                      <Card.Text>
+                        {data.desc}
+                      </Card.Text>
+                      <a href={data.link} className="btn btn-primary">Baca Selengkapnya</a>
+                    </Card.Body>
+                  </Card>
+                </div>
+              </Col>
+              );
+            })}
           </Row>
         </Container>
-      </>
-    );
-  };
-  
-  export default ArticleHome;
+      </section>
+    </>
+  );
+};
+
+export default ArticleHome;
