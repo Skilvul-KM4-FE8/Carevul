7f6c0f108c3c40f7bb119782a9634b5558616060
Nurul-Isma <nurulismawati236@gmail.com>
2023-06-09 18:45:55 +0700

    Menambahkan Register Page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import "./../styles/loginregis.css"
-import loginLogo from './../assets/login.svg'
+import regisLogo from './../assets/register.svg'
 import { Link } from "react-router-dom";
 
-function Login() {
+function Register() {
   return (
     <div>
     <section class="login d-flex">
@@ -11,24 +11,27 @@ function Login() {
         <div class="row justify-content-center align-items-center h-100">
           <div class="col-7">
             <div class="header">
-              <h1>Hallo, Carefriends</h1>
-              <p>Login dulu yuk sebelum kamu mengakses Carevul</p>
+              <h1>Daftar dulu yuk</h1>
+              <p>Gratis konsultasi di Carevul dimanapun dan kapanpun</p>
             </div>
             <div class="login-form">
 
-              <form id="form-login">
-                <label for="email" class="form-label">Email</label>
+              <form id="form-register">
+                <label for="name" class="form-label">Nama</label>
+                <input type="text" class="form-control" id="name" placeholder="Masukkan Nama" />
+
+                <label for="email" class="form-label mrgn-1">Email</label>
                 <input type="email" class="form-control" id="email" placeholder="Masukkan Email" />
 
                 <label for="password" class="form-label mrgn-1">Password</label>
                 <input type="password" class="form-control" id="password" placeholder="Masukkan Password" />
 
-                <button class="btn-2" type="submit">Login</button>
+                <button class="btn-regis" type="submit">Daftar</button>
               </form>
               <p class="fsize-15 d-block text-center">
-                Kamu belum punya akun?
-                <Link to={"/regis"} style={{ textDecoration: "none" }}>
-                <span class="regis">Daftar Disini</span>
+                Sudah punya akun?
+                <Link to={"/login"} style={{ textDecoration: "none" }}>
+                <span class="regis">Silahkan Login</span>
               </Link>
               </p>
             </div>
@@ -37,13 +40,12 @@ function Login() {
       </div>
       <div class="login-right w-50 h-100 bg-color">
         <div class="d-flex justify-content-center py-5">
-          <img src={loginLogo} alt="" />
+        <img src={regisLogo} alt="" />
         </div>
       </div>
     </section>
-
     </div>
   )
 }
 
-export default Login
+export default Register
