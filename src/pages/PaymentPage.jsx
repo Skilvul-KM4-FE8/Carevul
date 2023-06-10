a7e60d52a6e59eedc9290dd15bd1aa99f772f717
rafai123 <rafaimhd123@gmail.com>
2023-06-10 17:07:14 +0700

    add hard coded radio button for payment

--- src/pages/PaymentPage.jsx
+++ src/pages/PaymentPage.jsx
@@ -3,6 +3,11 @@ import Calendar from "react-calendar"
 // import 'react-calendar/dist/Calendar.css'
 import './../styles/Calendar.css'
 
+import gopayLogo from "./../assets/gopay.svg"
+import shopeepayLogo from "./../assets/shopeepay.svg"
+import ovoLogo from "./../assets/ovo.svg"
+import danaLogo from "./../assets/dana.svg"
+
 const PaymentPage = () => {
     return (
         <>
@@ -13,24 +18,118 @@ const PaymentPage = () => {
                         {/* <h1 className="text-carevul mb-4">Calendar</h1> */}
                         <Calendar />
                     </Col>
-                    
+                        
                     <Col>
-                        {/* Card Doctor's Info */}
-                        <Container className="shadow py-3 px-5 rounded">
-                            <Row>
-                                <Col className="d-flex justify-content-center align-items-center">
-                                <img src="https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/914.jpg" alt="doctor" className="rounded-circle" />
-                                </Col>
-                                <Col className="text-start mb-3" md={7} >
-                                <h4 className="text-carevul fw-bold ms-0 mt-2 doctor-name mt-n1">dr. Testt bin udin</h4>
-                                <p className=" doctor-name">Dokter Umum <br /> RS. Umum Isekai, Isekai, Indonesia</p>
-                                <h5 className=" doctor-name">  <span className="text-carevul ">Rp. 20.000</span></h5>
-                                </Col>
-                                <Col className="d-flex justify-content-center align-items-center">
-                                {/* <Link to={"/list-doctor/id"} className="btn text-white fw-bold color-carevul-gradient px-5 py-2 shadow-sm">Pilih Dokter</Link> */}
-                                </Col>
-                            </Row>
-                        </Container>
+
+                    <Container> 
+                        <Row>
+                            <Col>
+                                {/* Card Doctor's Info */}
+                                <Container className="shadow py-3 px-5 rounded">
+                                    <Row>
+                                        <Col className="d-flex justify-content-center align-items-center">
+                                        <img src="https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/914.jpg" alt="doctor" className="rounded-circle" />
+                                        </Col>
+                                        <Col className="text-start mb-3" md={7} >
+                                        <h4 className="text-carevul fw-bold ms-0 mt-2 doctor-name mt-n1">dr. Testt bin udin</h4>
+                                        <p className=" doctor-name">Dokter Umum <br /> RS. Umum Isekai, Isekai, Indonesia</p>
+                                        <h5 className=" doctor-name">  <span className="text-carevul ">Rp. 20.000</span></h5>
+                                        </Col>
+                                        <Col className="d-flex justify-content-center align-items-center">
+                                        {/* <Link to={"/list-doctor/id"} className="btn text-white fw-bold color-carevul-gradient px-5 py-2 shadow-sm">Pilih Dokter</Link> */}
+                                        </Col>
+                                    </Row>
+                                </Container>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col className="mt-5">
+                                <h4 className="text-carevul fw-bold">Pilih Metode Pembayaran</h4>
+                                <form >
+                                    {/* Gopay */}
+                                        <div className="form-check ms-0 py-3 px-5 border rounded mt-1 shadow-sm  border-2">
+                                            {/* Section Input */}
+                                                <div className="container">
+                                                    <div className="row">
+                                                        <div className="col-1 d-flex justify-content-center align-items-center">
+                                                            <input className="form-check-input" type="radio" name="book-date" id="gopay"  />
+                                                        </div>
+                                                        <div className="col">
+                                                            <label className="form-check-label" htmlFor="gopay">
+                                                                <p className="lead mt-3"><img src={gopayLogo} width={100} /></p>
+                                                                {/* <h5 className="text-carevul">Rp. 20000</h5> */}
+                                                            </label>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            {/* End Section Input */}
+                                        </div>
+                                    {/* End Gopay */}
+                                    {/* shopeepay */}
+                                        <div className="form-check ms-0 py-3 px-5 border rounded mt-1 shadow-sm  border-2">
+                                            {/* Section Input */}
+                                                <div className="container">
+                                                    <div className="row">
+                                                        <div className="col-1 d-flex justify-content-center align-items-center">
+                                                            <input className="form-check-input" type="radio" name="book-date" id="shopeepay"  />
+                                                        </div>
+                                                        <div className="col">
+                                                            <label className="form-check-label" htmlFor="shopeepay">
+                                                                <p className="lead mt-3"><img src={shopeepayLogo} width={100} /></p>
+                                                                {/* <h5 className="text-carevul">Rp. 20000</h5> */}
+                                                            </label>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            {/* End Section Input */}
+                                        </div>
+                                    {/* End shopeepay */}
+                                    {/* dana */}
+                                        <div className="form-check ms-0 py-3 px-5 border rounded mt-1 shadow-sm  border-2">
+                                            {/* Section Input */}
+                                                <div className="container">
+                                                    <div className="row">
+                                                        <div className="col-1 d-flex justify-content-center align-items-center">
+                                                            <input className="form-check-input" type="radio" name="book-date" id="dana"  />
+                                                        </div>
+                                                        <div className="col">
+                                                            <label className="form-check-label" htmlFor="dana">
+                                                                <p className="lead mt-3"><img src={danaLogo} width={100} /></p>
+                                                                {/* <h5 className="text-carevul">Rp. 20000</h5> */}
+                                                            </label>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            {/* End Section Input */}
+                                        </div>
+                                    {/* End dana */}
+                                    {/* ovo */}
+                                        <div className="form-check ms-0 py-3 px-5 border rounded mt-1 shadow-sm  border-2">
+                                            {/* Section Input */}
+                                                <div className="container">
+                                                    <div className="row">
+                                                        <div className="col-1 d-flex justify-content-center align-items-center">
+                                                            <input className="form-check-input" type="radio" name="book-date" id="ovo"  />
+                                                        </div>
+                                                        <div className="col">
+                                                            <label className="form-check-label" htmlFor="ovo">
+                                                                <p className="lead mt-3"><img src={ovoLogo} width={100} /></p>
+                                                                {/* <h5 className="text-carevul">Rp. 20000</h5> */}
+                                                            </label>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            {/* End Section Input */}
+                                        </div>
+                                    {/* End ovo */}
+                                    <div className="col-md-12 d-flex">
+                                        <button type="submit" className="btn color-carevul-gradient text-white mt-4 px-5 py-2 flex-fill" id="book-btn">Buat Janji</button>
+                                    </div>
+                                </form>
+                            </Col>
+                        </Row>
+                    </Container>
+                        
                     </Col>
                 </Row>
             </Container>
