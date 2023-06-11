0e3d7f420bd89d1749d0b680528ecff42280b518
alfito <alfitosantosa@gmail.com>
2023-06-11 15:25:35 +0700

    rapihin navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,43 +6,42 @@ import CarevulLogo from "../assets/carevul-logo.svg";
 import { Button } from "react-bootstrap";
 import "./../styles/Navbar.css";
 
-const NavbarComponent = () => {
+function BasicExample() {
   return (
-    <>
-      <br />
-      <Navbar bg="light" variant="light">
-        <Container>
-          <Navbar.Brand href="#">
-            {" "}
-            <div className="logoApp">
-              <img src={CarevulLogo} alt="Your SVG" />
-            </div>
-          </Navbar.Brand>
-
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#">
+          {" "}
+          <div className="logoApp">
+            <img src={CarevulLogo} alt="Carevul Logo" />{" "}
+          </div>{" "}
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <NavLink to={"/"} className="nav-link active">
-              Home
-            </NavLink>
+              Home{" "}
+            </NavLink>{" "}
             <NavLink to={"/paymentdoctor"} className="nav-link active">
-              Payment Test
-            </NavLink>
+              Payment Test{" "}
+            </NavLink>{" "}
             <NavLink to={"/listdoctor"} className="nav-link active">
-              List Doctor
-            </NavLink>
+              List Doctor{" "}
+            </NavLink>{" "}
             <NavLink to={"/testt"} className="nav-link active">
-              Menu klen
+              Menu klen{" "}
             </NavLink>
             <NavLink to={"/"} className="nav-link active">
-              <Button variant="outline-primary">Login</Button>
-            </NavLink>
+              <Button variant="outline-primary">Login</Button>{" "}
+            </NavLink>{" "}
             <NavLink to={"/"} className="nav-link active">
-              <Button variant="primary">Register</Button>
+              <Button variant="primary">Register</Button>{" "}
             </NavLink>
           </Nav>
-        </Container>
-      </Navbar>
-    </>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
-};
+}
 
-export default NavbarComponent;
+export default BasicExample;
