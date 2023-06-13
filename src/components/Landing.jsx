2dc62fcb31467f191b8caee3e485cdff65baf484
alfito <alfitosantosa@gmail.com>
2023-06-13 17:20:06 +0700

    mengganti character banner

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -6,11 +6,6 @@ import "../styles/Landing.css";
 import Pict1 from "../assets/pict1.png";
 import Pict2 from "../assets/pict2.png";
 import { Col, Container, Row } from "react-bootstrap";
-import fb from "../assets/icon/icons8-facebook.svg";
-import ins from "../assets/icon/icons8-instagram.svg";
-import lin from "../assets/icon/icons8-linked-in.svg";
-import twi from "../assets/icon/icons8-twitter.svg";
-import yt from "../assets/icon/icons8-youtube.svg";
 
 const Landing = () => {
   return (
@@ -33,7 +28,7 @@ const Landing = () => {
             </button>
           </div>
           <div className="hero-image">
-            <img src={CharacterBanner} className="Character" alt="" />
+            <img src={Pict1} className="Character" alt="" />
           </div>
         </div>
       </section>
