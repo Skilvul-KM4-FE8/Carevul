9228ab8ab16d413cd5958f61a370cdda1cda3123
Muhammad Rafai <rafaimhd123@gmail.com>
2023-06-15 02:10:58 +0700

    fixed all logic for chat

--- src/context/roomChatContext.jsx
+++ src/context/roomChatContext.jsx
@@ -3,13 +3,9 @@ import { createContext, useState } from "react";
 export const RoomChatContext = createContext();
 
 const RoomChatProvider = ({ children }) => {
-    const [roomChat, setRoomChat] = useState(false);
+  const [roomChat, setRoomChat] = useState();
 
-    return (
-        <RoomChatContext.Provider value={{ roomChat, setRoomChat }}>
-            {children}
-        </RoomChatContext.Provider>
-    );
+  return <RoomChatContext.Provider value={{ roomChat, setRoomChat }}>{children}</RoomChatContext.Provider>;
 };
 
 export default RoomChatProvider;
