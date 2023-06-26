8c5542a157cf44ed7112fb9f863f4a5fa0f1bf47
Muhammad Rafai <rafaimhd123@gmail.com>
2023-06-26 10:22:51 +0700

    fix aos for article

--- src/components/Cardticle.jsx
+++ src/components/Cardticle.jsx
@@ -26,7 +26,7 @@ function Cardticle() {
       {cards.map((item) => (
         <Col lg={{ span: 4, offset: 0 }} key={item.id} id="col-card">
           {/* </Col><Col md={4} key={item.id} id="col-card"> */}
-          <div data-aos="fade-right" data-aos-duration="1000">
+          <div data-aos="zoom-in" data-aos-duration="1000">
             <Card className="card" id="card"  >
               <Card.Img variant="top" src={item.img} id="card-img" />
               <Card.Body className="card-body">
